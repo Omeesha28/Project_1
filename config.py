75c606ea7a59db3723715ba2a7215350c231cd8f
Neil7777 <148916909+Neil7777@users.noreply.github.com>
2023-12-15 13:10:59 -0500

    updated file names and adding api_key in git ignore

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-RapidAPIKey = "99e7d6cf51msh4012312a35ec7d6p1a6f4djsn291c6e8f7944"
+RapidAPIKey = "abf6e536c5mshe48f5c209b10e4bp138eabjsn75280df5715c"
 geoapify_key = "61af9cc886364e8cb0382e25ca72cc9f"
 
 
